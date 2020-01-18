29b25616effe082010c4d1828289fc2e5aeae936
Mangenese <blakemangen20@gmail.com>
2020-01-17 23:57:52 -0700

    added the code for images to animate and stop when pressed, as well as for it to display in html. working on css changes now.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,8 +9,64 @@
 // array for the items to go into 
 var pokemon = ["Pikachu", "Charizard", "Greninja", "Eevee", "Squirtle", "Piplup" ]
 
-//function for displaying the gifs for the page 
+$(document).on("click", ".pokemon", function (){
+    
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + $(this).attr("data-name") + "&api_key=2GkeMiEVpWc9VEWUuwkVWAf36g5KM1ul&limit=10"
+    
+    $(".gif").empty();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        for (var i = 0; i < response.data.length; i++) {
+            
+            var rating = response.data[i].rating;
+            var imgDiv = $("<div>");
+            var image = $("<img>");
+            var p = $("<p>");
+           
+            imgDiv.addClass("img-div")
+            p.addClass("rating");
+            p.attr("src", rating);
+            // setting a class with the tage to animate it
+            image.attr("class", "animateGif")
+            image.attr("src", response.data[i].images.original_still.url)
+            image.attr("data-animate", response.data[i].images.original.url);
+            image.attr("data-still", response.data[i].images.original_still.url);
+            image.attr("data-state", "still");
+            image.attr("data-state", "animate");
+            //prepending the rating and buttons to the imgDiv
+            imgDiv.prepend("Rating: " + rating);
+            imgDiv.prepend(image);
+            //prepending the imgDiv to the class of gif
+            $(".gif").prepend(imgDiv);
 
+        }
+
+        
+        $(document).on("click", ".animateGif", function () {
+            var status = $(this).attr("data-state");
+            if (status === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            }
+        
+            else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+
+        })
+
+        
+
+    });
+
+})
+
+
+//function for rendering buttons 
 function renderButtons() {
     // to stop buttons from repeating 
     $("#buttons").empty();
@@ -26,5 +82,12 @@ function renderButtons() {
 
     }
 }
-
+// on click function that grabs the users input and makes a button
+$("#add-button").on("click", function(event) {
+    event.preventDefault();
+    var input = $("#button-input").val().trim();
+    pokemon.push(input);
+    renderButtons();
+});
+ 
 renderButtons();
