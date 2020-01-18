db4555dad2a56b86f184615adcf5e9131deb27c1
Mangenese <blakemangen20@gmail.com>
2020-01-17 18:58:41 -0700

    added a title to the html and added pseudocode to the js code

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//create an app that displays 10 images on a page with a click of the button
+// when user clicks on button
+// display 10 images when button is clicked 
+// user can then click on the button to animate the gif
+// user can also click it again to stop the gif 
+// allow the user to create a way to add a new button
+// that new button will populate into the array for topics 
